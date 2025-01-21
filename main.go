74fb901d0f121b82454049956d04a3dca2494095
asamatov <lcodecrafterl@gmail.com>
2025-01-21 19:52:37 +0500

    Added main, go.mod for generation and iota.go is a different project

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
-import "fmt"
-
-const (
-	one = iota*2 + 1
-	three
-	five
-	seven
-	nine
-	eleven
+import (
+	"fmt"
+	"gobasics/GoPractice"
 )
 
-// 1 3 5 7 9 11
 func main() {
-	fmt.Println(one, three, five, seven, nine, eleven)
+	var b int = 1970
+	fmt.Println(GoPractice.Generation(b))
 }
